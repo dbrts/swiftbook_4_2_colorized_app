df770e47c6615c7d93fb15634b2a5176521281c8
Gennady Dobretsov <dobrets@gmail.com>
2023-01-11 00:23:46 +0600

    Initial Commit

---
+++ swiftbook_4_2_colorized_app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swiftbook_4_2_colorized_app
+//
+//  Created by dobrets on 11.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftbook_4_2_colorized_app/swiftbook_4_2_colorized_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftbook_4_2_colorized_appApp.swift
+//  swiftbook_4_2_colorized_app
+//
+//  Created by dobrets on 11.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftbook_4_2_colorized_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
