8ef2c9abafd70d3348d0da30b2eafb1312f21152
Gennady Dobretsov <dobrets@gmail.com>
2023-01-12 00:26:44 +0600

    on tap hide keyboard

--- swiftbook_4_2_colorized_app/ContentView.swift
+++ swiftbook_4_2_colorized_app/ContentView.swift
@@ -11,40 +11,49 @@ struct ContentView: View {
     @State private var redSliderValue = 120.0
     @State private var greenSliderValue = 120.0
     @State private var blueSliderValue = 120.0
+    
     @FocusState var isInputActive: Bool
     
     var body: some View {
-        VStack {
-            RoundedRectangle(cornerRadius: 20)
-                .frame(height: 150)
-                .foregroundColor(
-                    Color(
-                        red: redSliderValue / 255,
-                        green: greenSliderValue / 255,
-                        blue: blueSliderValue / 255
-                    )
-                )
-                .padding(.bottom, 16)
+        ZStack{
+            Color(.lightGray)
+                .ignoresSafeArea()
+                .onTapGesture {
+                    isInputActive = false
+                }
             
             VStack {
-                ColorSliderView(value: $redSliderValue, color: .red)
-                ColorSliderView(value: $greenSliderValue, color: .green)
-                ColorSliderView(value: $blueSliderValue, color: .blue)
-            }
-            .focused($isInputActive)
-            .toolbar {
-                ToolbarItemGroup(placement: .keyboard) {
-                    Spacer()
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(height: 150)
+                    .foregroundColor(
+                        Color(
+                            red: redSliderValue / 255,
+                            green: greenSliderValue / 255,
+                            blue: blueSliderValue / 255
+                        )
+                    )
+                    .padding(.bottom, 16)
+                
+                VStack {
+                    ColorSliderView(value: $redSliderValue, color: .red)
+                    ColorSliderView(value: $greenSliderValue, color: .green)
+                    ColorSliderView(value: $blueSliderValue, color: .blue)
+                }
+                .focused($isInputActive)
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
 
-                    Button("Done") {
-                        isInputActive = false
+                        Button("Done") {
+                            isInputActive = false
+                        }
                     }
                 }
+                
+                Spacer()
             }
-            
-            Spacer()
+            .padding()
         }
-        .padding()
     }
 }
 
